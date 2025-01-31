13369f4ec21979360bef5152f613da75bb10734e
KChino <kevintlapizaga@gmail.com>
2025-01-31 01:05:04 -0300

    Add middleware

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import taskRoutes from "./routes/taskRoutes.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 
@@ -8,5 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/tasks", taskRoutes);
+
+app.use(errorHandler);
 
 export default app;

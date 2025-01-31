57e9a7c062872655f42c800e34a598d4b920e8b4
KChino <kevintlapizaga@gmail.com>
2025-01-31 01:04:40 -0300

    Implement taskController

--- backEnd/src/controllers/taskController.js
+++ backEnd/src/controllers/taskController.js
@@ -1,12 +1,49 @@
-let tasks = []; // Almacenamiento en memoria
-const lastId = null;
+import { v4 as uuidv4 } from "uuid";
+let tasks = []; // In-memory storage
+
+const generateNewId = () => {
+  return uuidv4();
+};
 
 export const getTasks = (req, res) => {
   res.json(tasks);
 };
 
-export const createTask = (req, res) => {};
+export const createTask = (req, res) => {
+  const { description, title } = req.body;
+  if (!title || !description) {
+    return res.status(400).json({
+      error: !title ? "El título es requerido" : "La descripción es requerida",
+    });
+  }
 
-export const completeTask = (req, res) => {};
+  const newTask = {
+    id: generateNewId(),
+    title,
+    description,
+    completed: false,
+  };
 
-export const deleteTask = (req, res) => {};
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+};
+
+export const completeTask = (req, res) => {
+  const { id } = req.params;
+  const task = tasks.find((t) => t.id === id);
+  if (!task) {
+    return res.status(404).json({ error: "Tarea no encontrada" });
+  }
+  task.completed = true;
+  res.json(task);
+};
+
+export const deleteTask = (req, res) => {
+  const { id } = req.params;
+  const index = tasks.findIndex((t) => t.id === id);
+  if (index === -1) {
+    return res.status(404).json({ error: "Tarea no encontrada" });
+  }
+  tasks.splice(index, 1);
+  res.status(204).send();
+};

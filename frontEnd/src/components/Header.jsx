acc86772702def379a512c6b77f0114f0bbcd176
KChino <kevintlapizaga@gmail.com>
2025-01-31 15:10:28 -0300

    Update Header.jsx

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
     <header className="text-center mb-6">
       <h1 className="text-4xl font-bold text-orange-500 tracking-wide flex items-center justify-center gap-2">
         TO-DO
-        <img src="/icon.svg" alt="Logo" className="w-6 h-6" />
+        <img src="/icon.svg" alt="Icon" className="w-6 h-6" />
       </h1>
     </header>
   );
